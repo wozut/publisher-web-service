14b9a395feeff072bbd28bf9ae24ea9bf0b39c3f
David Gracia <david.gracia.celemendi@gmail.com>
2025-09-06 00:54:47 +0200

    Refine Git hooks setup task to handle CI environment variable more accurately

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,8 @@ tasks.register("setupGitHooks", Exec::class) {
     description = "Setup Git hooks for all team members"
     group = "setup"
     commandLine("./scripts/setup-git-hooks.sh")
-    onlyIf { System.getenv("CI") == null }
+    val ciEnvironmentVariable = System.getenv("CI")
+    onlyIf { ciEnvironmentVariable == null || ciEnvironmentVariable != "true" }
 }
 
 tasks.named("build") {
