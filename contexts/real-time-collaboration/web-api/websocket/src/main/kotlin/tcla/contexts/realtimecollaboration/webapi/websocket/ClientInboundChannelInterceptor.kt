e692295c12dd76665a6fd24cede1754bc947237f
David Gracia Celemendi <david.gracia.celemendi@gmail.com>
2025-09-21 18:58:09 +0200

    Refactor WebSocket configuration: move `ClientInboundChannelInterceptor` to a separate file and update message broker subscription path to `/user`.

---
+++ contexts/real-time-collaboration/web-api/websocket/src/main/kotlin/tcla/contexts/realtimecollaboration/webapi/websocket/ClientInboundChannelInterceptor.kt
@@ -0,0 +1,72 @@
+package tcla.contexts.realtimecollaboration.webapi.websocket
+
+import org.springframework.messaging.Message
+import org.springframework.messaging.MessageChannel
+import org.springframework.messaging.simp.stomp.StompCommand
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor
+import org.springframework.messaging.support.ChannelInterceptor
+import org.springframework.messaging.support.MessageHeaderAccessor
+import org.springframework.stereotype.Component
+import java.time.Instant
+
+@Component
+class ClientInboundChannelInterceptor : ChannelInterceptor {
+
+    override fun preSend(message: Message<*>, channel: MessageChannel): Message<*> {
+        println("inbound preSend Thread name: ${Thread.currentThread().name}")
+        val accessor: StompHeaderAccessor? = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor::class.java)
+
+        if (StompCommand.CONNECT == accessor?.command) {
+            println("inbound preSend CONNECT")
+
+            // TODO: validate authentication here
+//            val authorization = accessor.getNativeHeader("Authorization")?.firstOrNull()
+            val userId: String? = accessor.getNativeHeader("UserId")?.firstOrNull()
+            println("inbound preSend getNativeHeader(\"UserId\"): $userId")
+            accessor.sessionAttributes["userId"] = userId
+            println("inbound preSend sessionAttributes userId ${accessor.sessionAttributes?.get("userId")}")
+
+//            if (authorization?.startsWith("Bearer ") == true) {
+//                val token = authorization.substring(7)
+//                accessor.sessionAttributes["userId"] = token
+//                println("preSend Authorization: $token")
+//                try {
+////                    validateJwtToken(token)
+//                } catch (e: Exception) {
+//                    throw RuntimeException("Invalid JWT token")
+//                }
+//            } else {
+//                throw RuntimeException("Missing Authorization header")
+//            }
+        }
+
+        if(StompCommand.SUBSCRIBE == accessor?.command) {
+            println("inbound preSend SUBSCRIBE")
+            println("sessionAttributes userId ${accessor.sessionAttributes["userId"]}")
+        }
+
+        if(StompCommand.SEND == accessor?.command) {
+            println("inbound preSend SEND. Time: ${Instant.now()}. Thread: ${Thread.currentThread().name}. Destination ${accessor.destination}")
+//            println("sessionAttributes userId ${accessor.sessionAttributes["userId"]}")
+        }
+
+        if(StompCommand.UNSUBSCRIBE == accessor?.command) {
+            println("inbound preSend UNSUBSCRIBE")
+            val userId = accessor.sessionAttributes?.get("userId") as? String
+            println("User $userId unsubscribed from subscription: ${accessor.subscriptionId}")
+            // Ejecutar lógica cuando se desuscribe
+        }
+
+        if(StompCommand.DISCONNECT == accessor?.command) {
+            println("inbound preSend DISCONNECT")
+            val userId = accessor.sessionAttributes?.get("userId") as? String
+            println("User $userId disconnected")
+            // Ejecutar lógica cuando se desconecta
+        }
+
+        val destination: String? = accessor?.getNativeHeader("destination")?.firstOrNull()
+        println("inbound preSend getNativeHeader(\"destination\"): $destination")
+
+        return message
+    }
+}
--- contexts/real-time-collaboration/web-api/websocket/src/main/kotlin/tcla/contexts/realtimecollaboration/webapi/websocket/WebSocketConfiguration.kt
+++ contexts/real-time-collaboration/web-api/websocket/src/main/kotlin/tcla/contexts/realtimecollaboration/webapi/websocket/WebSocketConfiguration.kt
@@ -1,19 +1,11 @@
 package tcla.contexts.realtimecollaboration.webapi.websocket
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.messaging.Message
-import org.springframework.messaging.MessageChannel
 import org.springframework.messaging.simp.config.ChannelRegistration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
-import org.springframework.messaging.simp.stomp.StompCommand
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor
-import org.springframework.messaging.support.ChannelInterceptor
-import org.springframework.messaging.support.MessageHeaderAccessor
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
-import org.springframework.stereotype.Component
-import java.time.Instant
 
 
 @Configuration
@@ -23,7 +15,7 @@ class WebSocketConfiguration(
 ) : WebSocketMessageBrokerConfigurer {
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
-        registry.enableSimpleBroker("/topic", "/queue")
+        registry.enableSimpleBroker("/topic", "/user")
         registry.setApplicationDestinationPrefixes("/app")
     }
 
@@ -35,67 +27,5 @@ class WebSocketConfiguration(
 
     override fun configureClientInboundChannel(registration: ChannelRegistration) {
         registration.interceptors(clientInboundChannelInterceptor)
-    }
-}
-
-@Component
-class ClientInboundChannelInterceptor : ChannelInterceptor {
-
-    override fun preSend(message: Message<*>, channel: MessageChannel): Message<*> {
-        println("preSend Thread name: ${Thread.currentThread().name}")
-        val accessor: StompHeaderAccessor? = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor::class.java)
-
-        if (StompCommand.CONNECT == accessor?.command) {
-            println("preSend CONNECT")
-
-            // TODO: validate authentication here
-//            val authorization = accessor.getNativeHeader("Authorization")?.firstOrNull()
-            val userId: String? = accessor.getNativeHeader("UserId")?.firstOrNull()
-            println("preSend getNativeHeader(\"UserId\"): $userId")
-            accessor.sessionAttributes["userId"] = userId
-            println("preSend sessionAttributes userId ${accessor.sessionAttributes?.get("userId")}")
-
-//            if (authorization?.startsWith("Bearer ") == true) {
-//                val token = authorization.substring(7)
-//                accessor.sessionAttributes["userId"] = token
-//                println("preSend Authorization: $token")
-//                try {
-////                    validateJwtToken(token)
-//                } catch (e: Exception) {
-//                    throw RuntimeException("Invalid JWT token")
-//                }
-//            } else {
-//                throw RuntimeException("Missing Authorization header")
-//            }
-        }
-
-        if(StompCommand.SUBSCRIBE == accessor?.command) {
-            println("preSend SUBSCRIBE")
-            println("sessionAttributes userId ${accessor.sessionAttributes["userId"]}")
-        }
-
-        if(StompCommand.SEND == accessor?.command) {
-            println("preSend SEND. Time: ${Instant.now()}. Thread: ${Thread.currentThread().name}. Destination ${accessor.destination}")
-//            println("sessionAttributes userId ${accessor.sessionAttributes["userId"]}")
-        }
-
-        if(StompCommand.UNSUBSCRIBE == accessor?.command) {
-            println("preSend UNSUBSCRIBE")
-            val userId = accessor.sessionAttributes?.get("userId") as? String
-            println("User $userId unsubscribed from subscription: ${accessor.subscriptionId}")
-            // Ejecutar lógica cuando se desuscribe
-        }
-
-        if(StompCommand.DISCONNECT == accessor?.command) {
-            println("preSend DISCONNECT")  
-            val userId = accessor.sessionAttributes?.get("userId") as? String
-            println("User $userId disconnected")
-            // Ejecutar lógica cuando se desconecta
-        }
-
-        val destination: String? = accessor?.getNativeHeader("destination")?.firstOrNull()
-        println("preSend getNativeHeader(\"destination\"): $destination")
-
-        return message
     }
 }
